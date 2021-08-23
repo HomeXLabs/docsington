d403e73bae212cef6f0b8fe739b05fcff78cb722
Eric Jalbert <ericmjalbert@gmail.com>
2021-08-23 14:11:08 -0400

    feat: add a bunch of random stuff

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const docsington = require('./src/main');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // TODO this input needs to be updated to match docsington
+    const words = core.getInput('words');
+    core.info(`printing ${words} ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
+    await docsington(words);
     core.info((new Date()).toTimeString());
 
+    // TODO change this to be a real output for docsington
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
--- index.test.js
+++ index.test.js
@@ -1,23 +1,21 @@
-const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
+const docsington = require('./src/main');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+test('throws invalid words', async () => {
+  await expect(docsington(10213)).rejects.toThrow('Value of words is not a string');
 });
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
+test('check returns "hello world"', async () => {
+  docsington('hello world').then(function(result) {
+    expect(result).toBe('hello world');
+  });
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
+  process.env['INPUT_WORDS'] = 'hello test runs!';
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
 })
